aacc1f939318ed3c2414701975966b739f687505
Howard <howardjer@gmail.com>
2020-11-14 12:51:13 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var tweets = [
+    'COVID just keeps on going',
+    'Trump is still acting up, what did you expect',
+    'Will we be abandoning the Keystone Pipe Line now',
+    'Still not use to Alberta cold',
+    'Next 6 months; shovel sidewalk, shovel sidewalk, shovel sidewalk',
+    'Why do they put braille on drive-thru atm keypads',
+    "My feet hurt, buy right sized shoes",
+    "Rode a bull named Bodacious for 8 seconds",
+    "Empty and dark shall I raise my lantern",
+    "Let he hath steerage of my course, direct my sails",
+    "With great power comes great responsibility",
+
+    
+];
+
+
+
+// document.write("<ul>");
+// var counter = 0;
+// while(counter < 6) {
+//     document.write("<li>" + animals[counter] + "</li>");
+//     counter++;
+// }
+// document.write("</ul>");
+
+
+
+for(var counter = 0; counter < tweets.length; counter++) {
+    ("<li>" + tweets[counter] + "</li>");
+    console.log (tweets[counter])
+
+}
+
+
+
+
+
+
